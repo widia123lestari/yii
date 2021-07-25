d6d8364e768d4642dc66d2ca11d56da4f4780b87
Widia Lestari <simetapuspi@gmail.com>
2021-07-25 21:39:30 +0700

    Change Tanggal Lahir input using DatePicker

--- views/mahasiswa/_form.php
+++ views/mahasiswa/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\date\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Mahasiswa */
@@ -16,7 +17,15 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'jenis_kelamin')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'tanggal_lahir')->textInput() ?>
+    <?= $form->field($model, 'tanggal_lahir')->widget(DatePicker::classname(),[
+    'name' => 'dp_2',
+    'type' => DatePicker::TYPE_COMPONENT_PREPEND,
+    'value' => '2000-12-02',
+    'pluginOptions' => [
+        'autoclose'=>true,
+        'format' => 'yyyy-mm-dd'
+    ]
+    ]); ?>
 
     <?= $form->field($model, 'id_provinsi')->textInput() ?>
 
